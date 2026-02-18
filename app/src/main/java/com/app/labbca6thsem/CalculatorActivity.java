7e7bd90304b79348d505498ba70a188f2fad1220
Nucleus <deadkillerzenith@gmail.com>
2026-02-18 21:32:58 +0545

    Lab 2: Develop an android app to make a simple calculator.

---
+++ app/src/main/java/com/app/labbca6thsem/CalculatorActivity.java
@@ -0,0 +1,93 @@
+package com.app.labbca6thsem;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    EditText etNum1, etNum2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+        etNum1 = findViewById(R.id.et_num1);
+        etNum2 = findViewById(R.id.et_num2);
+        btnAdd = findViewById(R.id.btn_add);
+        btnSub = findViewById(R.id.btn_sub);
+        btnMul = findViewById(R.id.btn_mul);
+        btnDiv = findViewById(R.id.btn_div);
+        tvResult = findViewById(R.id.tv_result);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('+');
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('-');
+            }
+        });
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('*');
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('/');
+            }
+        });
+    }
+
+    private void calculate(char operator) {
+        String num1Str = etNum1.getText().toString();
+        String num2Str = etNum2.getText().toString();
+
+        if (num1Str.isEmpty() || num2Str.isEmpty()) {
+            Toast.makeText(this, "Please enter both numbers", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double num1 = Double.parseDouble(num1Str);
+        double num2 = Double.parseDouble(num2Str);
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                result = num1 / num2;
+                break;
+        }
+
+        tvResult.setText("Result: " + result);
+    }
+}
